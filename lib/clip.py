375d8b4a457405195a21b358a14d491aadf73377
omondimaurine <omondimaurine713@gmail.com>
2024-06-12 14:56:41 +0300

    initializing our database and defining add customer function

--- lib/clip.py
+++ lib/clip.py
@@ -2,3 +2,12 @@ import argparse
 import sys
 from main import Base, engine, session
 from main import Customer, Bike, Rental
+
+def init_db():
+    Base.metadata.create_all(engine)
+
+def add_customer(name, email, rental_history=None):
+    new_customer = Customer(name=name, email=email, rental_history=rental_history)
+    session.add(new_customer)
+    session.commit()
+    print(f"Added customer {name}")
