9306da236ea2c11c477aaba7a9740e2fe53e1edc
omondimaurine <omondimaurine713@gmail.com>
2024-06-12 14:43:48 +0300

    creating a Rental class

---
+++ lib/main.py
@@ -0,0 +1,24 @@
+from sqlalchemy import create_engine, ForeignKey, Column, String, Integer, Date
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy.dialects.sqlite import INTEGER, TEXT
+
+Base = declarative_base()
+
+class Rental(Base):
+    __tablename__ = 'rentals'
+    
+    id = Column(INTEGER, primary_key=True)
+    rental_date = Column(Date)
+    return_date = Column(Date)
+    rental_duration = Column(INTEGER)
+    customer_name = Column(String, ForeignKey('customers.name'))
+    bike_id = Column(INTEGER, ForeignKey('bikes.id'))
+
+    customer = relationship("Customer", back_populates="rentals")
+    bike = relationship("Bike", back_populates="rentals")
+
+
+
+Session = sessionmaker(bind=engine)
+session = Session()
